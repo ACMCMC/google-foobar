8e6ed123b560e6f4c5e611d7cddf85cfae6062d6
Creo Mariño Aldán <aldan.creo@rai.usc.es>
2020-07-13 15:31:39 +0000

    Changed the TODO

--- src/scrape.py
+++ src/scrape.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 
 SEARCH_URL = "https://www.google.com/search?q=" #this is the base URL, to which we have to join the search query
 
+#TODO: Implement the web scraper with Selenium instead of requests+bautifulsoup, because even though they do get the webpage, we need the user to be logged in his Google account for him to be able to access the Google Foobar challenge again. Selenium uses the standard OS browser, so it's better that way.
+
 def scrape(search_query): #search_query is a string of words we want to use to search for (related to programming questions), separated by spaces
 
     #load the search URL
